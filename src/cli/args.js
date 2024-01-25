3a1770b1fb9dfbd5c811d4cd373c308c21159ee2
ruslan-kupzhassarov <Ruslan_Kupzhassarov@epam.com>
2024-01-25 21:08:57 +0600

    feat: cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const props = process.argv.slice(2);
+  const keys = props.filter((key) => key.startsWith("--"));
+  const values = props.filter((key) => !key.startsWith("--"));
+  const output = keys
+    .map((item, index) => `${item.replace("--", "")} is ${values[index]}`)
+    .join(", ");
+  console.log(output);
 };
 
 parseArgs();
