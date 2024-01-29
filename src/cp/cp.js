a433f9f839318b9c054db3eeef496f839f6d0a7b
ruslan-kupzhassarov <Ruslan_Kupzhassarov@epam.com>
2024-01-29 19:38:59 +0600

    feat: cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,10 @@
+import { fork } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork("./files/script.js", args);
+  child.on("message", (message) => {
+    console.log("Result: ", message);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
