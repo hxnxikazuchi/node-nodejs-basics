370b62928f640a5ed29b93134027ad98342184aa
hxnxikazuchi <kupzhasarov14@gmail.com>
2024-01-29 21:38:22 +0600

    Revert "feat: cli task"
    This reverts commit 3a1770b1fb9dfbd5c811d4cd373c308c21159ee2.

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,11 +1,5 @@
 const parseArgs = () => {
-  const props = process.argv.slice(2);
-  const keys = props.filter((key) => key.startsWith("--"));
-  const values = props.filter((key) => !key.startsWith("--"));
-  const output = keys
-    .map((item, index) => `${item.replace("--", "")} is ${values[index]}`)
-    .join(", ");
-  console.log(output);
+    // Write your code here 
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,5 @@
 const parseEnv = () => {
-  const rssVars = Object.keys(process.env).filter((key) =>
-    key.startsWith("RSS_")
-  );
-  const output = rssVars.map((key) => `${key}=${process.env[key]}`).join("; ");
-
-  console.log(output);
+    // Write your code here 
 };
 
 parseEnv();
