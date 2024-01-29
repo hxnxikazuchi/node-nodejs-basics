8246845b041a7ce626c47f040b94abeb127af16c
hxnxikazuchi <kupzhasarov14@gmail.com>
2024-01-29 21:41:24 +0600

    Revert "feat: zlib task"
    This reverts commit 0afe0ffabb017218ade95f415c1867cafb5fbd57.

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,17 +1,5 @@
-import { createGzip } from "node:zlib";
-import { pipeline } from "node:stream/promises";
-import { createReadStream, createWriteStream } from "fs";
-
 const compress = async () => {
-  try {
-    const gZip = createGzip();
-    const file = createReadStream("./files/fileToCompress.txt");
-    const dest = createWriteStream("./files/archive.gz");
-
-    await pipeline(file, gZip, dest);
-  } catch (error) {
-    console.error(error);
-  }
+    // Write your code here 
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,17 +1,5 @@
-import { createGunzip } from "zlib";
-import { pipeline } from "node:stream/promises";
-import { createReadStream, createWriteStream } from "fs";
-
 const decompress = async () => {
-  try {
-    const unzip = createGunzip();
-    const zipFile = createReadStream("./files/archive.gz");
-    const unzippedFile = createWriteStream("./files/fileToCompress.txt");
-    
-    await pipeline(zipFile, unzip, unzippedFile);
-  } catch (err) {
-    console.error(err);
-  }
+    // Write your code here 
 };
 
 await decompress();
