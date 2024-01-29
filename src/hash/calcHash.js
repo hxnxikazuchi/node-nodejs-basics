18fa1aae275a91694302e2b9e293050248350b16
hxnxikazuchi <kupzhasarov14@gmail.com>
2024-01-29 21:38:57 +0600

    Revert "feat: hash task"
    This reverts commit 4c3028fd41ae02a2e9caebdb3a54d866880a361b.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,22 +1,5 @@
-import { createHash } from "node:crypto";
-import { createReadStream } from "fs";
-
-const calculateHash = async () => {  
-  const hash = createHash('sha256');
-  const stream = createReadStream('./files/fileToCalculateHashFor.txt');
-
-  stream.on('data', (chunk) => {
-      hash.update(chunk);
-  });
-
-  stream.on('end', () => {
-      const fileHash = hash.digest('hex');
-      console.log(fileHash);
-  });
-
-  stream.on('error', (err) => {
-      console.error('Error reading the file:', err);
-  });
+const calculateHash = async () => {
+    // Write your code here 
 };
 
 await calculateHash();
